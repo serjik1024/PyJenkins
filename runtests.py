163c8d4c575bc2252fd880f4f455172db903f40e
nagenn <44678680+nagenn@users.noreply.github.com>
2020-11-27 11:54:37 +0530

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -3,20 +3,22 @@ import xmlrunner
 import json
 from book import Book
 
+books = Book()
 
 def runner(output='python_tests_xml'):
     return xmlrunner.XMLTestRunner(
         output=output
         )
 
-
-books = Book()
-
-def test_add():
-    result = books.add_book('Book1', 'Author1')
-    assert result
+class TestBook(unittest.TestCase):
+	
+	def test_add(self):
+		result = books.add_book('Book1', 'Author1')
+		print(result)
+		assert result
 
 if __name__ == '__main__':
-    print("Starting the test")
-    runner().run(test_add)
+    print("Starting the runner test")
+    runner().run(unittest.main())
+    #unittest.main()
     print("completed")
