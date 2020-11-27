0212eb2c783c61ba60af9e44eebf96327fc31816
nagenn <44678680+nagenn@users.noreply.github.com>
2020-11-27 10:53:52 +0530

    Create runtests.py

---
+++ runtests.py
@@ -0,0 +1,14 @@
+import unittest
+import xmlrunner
+
+
+def runner(output='python_tests_xml'):
+    return xmlrunner.XMLTestRunner(
+        output=output
+        )
+
+def find_tests():
+    return unittest.TestLoader().discover('.', 'test_addbook.py')
+if __name__ == '__main__':
+    runner().run(find_tests())
+    print("completed")
