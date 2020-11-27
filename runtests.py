a67df2a44f8d434f11c366a921aba1cfb4e55a83
nagenn <44678680+nagenn@users.noreply.github.com>
2020-11-27 11:22:03 +0530

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -9,7 +9,7 @@ def runner(output='python_tests_xml'):
 
 def find_tests():
     print("FInding the test")
-    return unittest.TestLoader().discover('/home/labsuser/jenkins/workspace/PyJ', 'test_addbook.py')
+    return unittest.TestLoader().discover('/home/labsuser/jenkins/workspace/PyJ', 'test_*.py')
 if __name__ == '__main__':
     print("Starting the test")
     runner().run(find_tests())
